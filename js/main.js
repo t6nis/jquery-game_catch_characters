e085c61016584e8217fc7db2d519564f64bbc7ae
T6nis <t6nis20@gmail.com>
2012-10-30 14:06:01 +0200

    30.10.2012 - optimizing script

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {  
     
-    var str_arr = ['H', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd', '!']; //correct order to look
+    var str_arr = ['H', 'e', 'l', 'l', 'o', '', 'w', 'o', 'r', 'l', 'd', '!']; //correct order to look
     var cur_pos = 0; //current position of checking
 
-    for (var i = 1; i <= 15; i++) {
+    for (var i = 0; i <= (str_arr.length-1); i++) {
         var rx = Math.ceil(Math.random()*550);
         var ry = Math.ceil(Math.random()*350);
         var hue = 'rgb('
             + (Math.floor(Math.random() * 256)) + ','
             + (Math.floor(Math.random() * 256)) + ','
             + (Math.floor(Math.random() * 256)) + ')';
+     
+        //if using sentences, empty array variables are spaces, so skip them
+        if (str_arr[i] == '') {
+            continue;
+        }     
+        
+        $('#wrapper').append('<div id="char'+i+'" class="char">'+str_arr[i]+'</div>');
         //Random positioning
         $('#char'+i).css('left', rx);
         $('#char'+i).css('top', ry);
@@ -17,12 +24,17 @@ $(document).ready(function() {
         $('#char'+i).css('color', hue);
     }
 
-    for (var i = 1; i <= 15; i++) {
+    for (var i = 0; i <= (str_arr.length-1); i++) {
         $('#char'+i).mouseover(function() {                    
             var dWidth = $('#wrapper').width() - 48, //font size of char (+10px just in case some characters like j,g,p might go over borders)
                 dHeight = $('#wrapper').height() - 48, //font size of char (+10px just in case some characters like j,g,p might go over borders)
                 nextX = Math.floor(Math.random() * dWidth),
-                nextY = Math.floor(Math.random() * dHeight);
+                nextY = Math.floor(Math.random() * dHeight);                
+                //append spaces when using sentences
+                if (str_arr[cur_pos] == '') {                    
+                    $('#result').append('&nbsp;');
+                    cur_pos++;
+                }
                 //if hovering over wrong char, move char away
                 if (this.innerHTML != str_arr[cur_pos]) {
                     $(this).animate({ left: nextX + 'px', top: nextY + 'px' });
@@ -32,11 +44,7 @@ $(document).ready(function() {
             if (this.innerHTML == str_arr[cur_pos]) {
                 //remove from game and add to result
                 $('#'+this.getAttribute('id')).remove();
-                $('#result').append(this.innerHTML);
-                //append spaces when using sentences
-                if (cur_pos == 4) {
-                    $('#result').append('&nbsp;');
-                }
+                $('#result').append(this.innerHTML);               
                 cur_pos++;
             }
         });
